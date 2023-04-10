6ee9b574b9ed1932b29024e7ddb1c49719f06d29
Guillaume Lavignotte <guillaume@moneytrack.io>
2023-04-10 20:41:48 +0200

    updated core files

---
+++ lib/app.dart
@@ -0,0 +1,43 @@
+import 'package:alx_technical_test/ui/pages/dashboard_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'blocs/characters_bloc.dart';
+import 'data/repositories/character_repository.dart';
+import 'ui/pages/characters_page.dart';
+
+class App extends StatelessWidget {
+  final CharacterRepository repository;
+
+  const App({Key? key, required this.repository}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CharactersBloc>(
+          create: (BuildContext context) {
+            return CharactersBloc(repository: repository);
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'ALX Technical Test',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const DefaultTabController(
+          animationDuration: Duration.zero,
+          length: 2,
+          child: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              CharactersPage(),
+              DashboardPage(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
